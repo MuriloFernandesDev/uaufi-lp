8802b275d5e748eaca3c493d7ef4fec3ae83aa04
Murilo Fernandes <muriloflimaa@gmail.com>
2023-07-13 10:30:54 -0300

    Corrigindo erro app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,11 @@ import { Theme } from 'react-daisyui';
 import '../styles/global.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+   const MyComponent = Component as any;
+
    return (
       <Theme dataTheme='light' className='bg-base-100'>
-         <Component {...pageProps} />
+         <MyComponent {...pageProps} />
       </Theme>
    );
 };
